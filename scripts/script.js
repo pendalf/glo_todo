23bc65b83ac56ba5cdc6cb4225806d57541a9072
Zander <akma_2003@mail.ru>
2021-02-12 09:20:31 +0300

    Выполнение урока №12 по ТЗ:
    3) Пустые дела добавляться не должны
    
    4) Поле ввода после добавления дела должно очищаться
    
    5) Удаление дел на кнопку КОРЗИНА
    
    6) Сохранять данные о делах в localStorage (советую в виде массива)
    
    7) Дела из localStorage подгружаться должны автоматически при загрузки странице

--- scripts/script.js
+++ scripts/script.js
@@ -5,15 +5,20 @@ const todoControl = document.querySelector('.todo-control'),
     todoList = document.querySelector('.todo-list'),
     todoCompleted = document.querySelector('.todo-completed');
 
-const todoData = [];
+let todoData = localStorage.todoList ? JSON.parse(localStorage.todoList) : [];
+
+const setLocalStorage = function() {
+    localStorage.todoList = JSON.stringify(todoData);
+};
 
 const render = function() {
     todoList.textContent = '';
     todoCompleted.textContent = '';
 
-    todoData.forEach(function(item) {
+    todoData.forEach(function(item, i) {
         const li = document.createElement('li');
         li.classList.add('todo-item');
+        li.dataset.index = i;
 
         li.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
             '<div class="todo-buttons">' +
@@ -28,25 +33,37 @@ const render = function() {
         }
 
         const todoComplete = li.querySelector('.todo-complete');
+        const todoremove = li.querySelector('.todo-remove');
 
         todoComplete.addEventListener('click', function() {
             item.completed = !item.completed;
+            setLocalStorage();
             render();
-        })
+        });
+        todoremove.addEventListener('click', function(event) {
+            const index = event.target.closest('li').dataset.index;
+            todoData.splice(index, 1);
+            setLocalStorage();
+            render();
+        });
     });
 };
 
 todoControl.addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const newTodo = {
-        value: headerInput.value,
-        completed: false
-    };
+    if (headerInput.value !== '') {
+        const newTodo = {
+            value: headerInput.value,
+            completed: false
+        };
 
-    todoData.push(newTodo);
+        todoData.push(newTodo);
+        headerInput.value = '';
 
-    render();
+        setLocalStorage();
+        render();
+    }
 });
 
 render();
